1302fc3e23f76eea80d4f27169d6b16f0b9c759b
lytsy <hanecin@gmail.com>
2021-11-10 01:05:52 +0200

    add sdl_image

---
+++ src/headers/sdl_image.h
@@ -0,0 +1,96 @@
+
+#define SDL_MAIN_HANDLED
+#include "../../vendor/sdl/include/SDL2/SDL.h"
+#include "../../vendor/sdl_image/include/SDL2/SDL_image.h"
+#include <stdio.h>
+
+class SDL_Img
+{
+public:
+    SDL_Img(const char *file, SDL_Renderer *sdl_renderer)
+    {
+        filename = (char *)file;
+        renderer = sdl_renderer;
+        _load_texture();
+        _init_src();
+        _init_dest();
+    }
+
+    void set_src(int x, int y, int w, int h)
+    {
+        src.x = x;
+        src.y = y;
+        src.w = w;
+        src.h = h;
+        p_src = &src;
+    };
+
+    void reset_src()
+    {
+        p_src = NULL;
+    };
+
+    void set_dest(int x, int y, int w, int h)
+    {
+        dest.x = x;
+        dest.y = y;
+        dest.w = w;
+        dest.h = h;
+        p_dest = &dest;
+    };
+
+    void reset_dest()
+    {
+        p_dest = NULL;
+    };
+
+    void draw()
+    {
+        SDL_RenderCopy(renderer, texture, p_src, p_dest);
+    };
+
+    void destroy()
+    {
+        SDL_DestroyTexture(texture);
+    }
+
+private:
+    char *filename;
+    SDL_Renderer *renderer;
+    SDL_Texture *texture;
+    SDL_Rect src;
+    SDL_Rect dest;
+    SDL_Rect *p_src;
+    SDL_Rect *p_dest;
+
+    void _load_texture()
+    {
+        SDL_Surface *surface = IMG_Load(filename);
+        if (!surface)
+        {
+            printf("Failed IMG_Load: %s\n", SDL_GetError());
+        }
+        texture = SDL_CreateTextureFromSurface(renderer, surface);
+        if (!texture)
+        {
+            printf("Failed CreateTexture: %s\n", SDL_GetError());
+        }
+        SDL_FreeSurface(surface);
+    };
+
+    void _init_src()
+    {
+        src.x = 0;
+        src.y = 0;
+        SDL_QueryTexture(texture, NULL, NULL, &src.w, &src.h);
+        p_src = NULL;
+    }
+
+    void _init_dest()
+    {
+        dest.x = 0;
+        dest.y = 0;
+        SDL_QueryTexture(texture, NULL, NULL, &dest.w, &dest.h);
+        p_dest = &dest;
+    }
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 
 #include "headers/sdl_controller.h"
 #include "headers/sdl_sound.h"
+#include "headers/sdl_image.h"
 #include "headers/main.h"
 
 int main(int argc, char **argv)
@@ -8,8 +9,12 @@ int main(int argc, char **argv)
     hide_console();
     sdl.init();
 
-    SDL_Sound sound("assets/sound.mp3");
-    sound.play();
+    SDL_Sound test_sound("assets/sound.mp3");
+    test_sound.play();
+    test_sound.destroy();
+
+    SDL_Img test_img("assets/test_img.png", sdl.renderer);
+    test_img.set_dest(20, 20, 50, 50);
 
     bool running = true;
     while (running)
@@ -27,7 +32,14 @@ int main(int argc, char **argv)
                 break;
             }
         }
+
+        SDL_RenderClear(sdl.renderer);
+        test_img.draw();
+        SDL_RenderPresent(sdl.renderer);
+        SDL_Delay(16);
     }
+
+    test_img.destroy();
     sdl.destroy();
     return 0;
 }
