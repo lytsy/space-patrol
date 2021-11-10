6647d7c229f0f256560e6c1a91a8fb0218c1233b
lytsy <hanecin@gmail.com>
2021-11-10 23:30:46 +0200

    rework hide_console

--- src/engine/console.h
+++ src/engine/console.h
@@ -1,17 +1,12 @@
 #include "config.h"
-void _hide_win32_console();
 
 #if defined(_WIN32) || defined(WIN32)
 #include <windows.h>
-void _hide_win32_console()
-{
-    ::ShowWindow(::GetConsoleWindow(), SW_HIDE);
-}
 #endif
 
 void hide_console()
 {
 #if (defined(_WIN32) || defined(WIN32)) && !DEVELOPMENT_MODE
-    _hide_win32_console();
+    ::ShowWindow(::GetConsoleWindow(), SW_HIDE);
 #endif
 }
