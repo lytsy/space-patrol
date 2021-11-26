bfa89358fee3850ec05090fe5d963d395fed56ba
lytsy <hanecin@gmail.com>
2021-11-27 01:23:34 +0200

    update icons, title

--- src/engine/config.h
+++ src/engine/config.h
@@ -1,6 +1,6 @@
 #define WINDOW_WIDTH 720
 #define WINDOW_HEIGHT 480
-#define WINDOW_TITLE "MyApp"
+#define WINDOW_TITLE "Game"
 #define WINDOW_ICON "assets/icons/favicon.png"
 #define WINDOW_FONT "assets/fonts/sans.ttf"
 #define WINDOW_FONT_SIZE 16
